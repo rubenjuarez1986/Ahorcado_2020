dc4a7d53deef7113bb6125def0245a66ce8a0fa8
rubenjuarez1986 <rubenjuaeli@gmail.com>
2020-03-20 11:03:55 +0100

    terminado(elige distintas palabras,cambio de fondo y imagenes)

--- Ahorcado_2020/Form1.cs
+++ Ahorcado_2020/Form1.cs
@@ -13,16 +13,29 @@ namespace Ahorcado_2020
     public partial class Form1 : Form
     {
         int numFallos = 0;
-        String palabraOculta = "CETYS";
+        String palabraOculta = eligePalabra();
         Boolean partidaTerminada = false;
 
         public Form1()
         {
             InitializeComponent();
+            String _palabraGuiones = "";
+            for (int i = 0; i < palabraOculta.Length; i++)
+            {
+                if (palabraOculta[i] != ' ')
+                {
+                    _palabraGuiones += "_ ";
+                }
+                else
+                {
+                    _palabraGuiones += "  ";
+                }
+                palabraGuiones.Text = _palabraGuiones;
+            }
         }
 
 
-        public String eligePalabra()
+        public static String eligePalabra()
         {
             String[] listaPalabras = { "HOLA", "MOÑECA", "OTOBUS","ESPATATRAPO", "COBETE","INDICION" };
             Random aleatorio = new Random();
@@ -35,16 +48,21 @@ namespace Ahorcado_2020
             Button boton = (Button)sender;
             String letra = boton.Text;
             letra = letra.ToUpper();
-
             if (palabraOculta.Contains(letra))
             {
-                int posicion = palabraOculta.IndexOf(letra);
-                palabraGuiones.Text = palabraGuiones.Text.Remove(2 * posicion, 1).Insert(2 * posicion, letra);
+                for (int i = 0; i < palabraOculta.Length; i++)
+                {
+                    if (palabraOculta[i] == letra[0])
+                    {
+                        palabraGuiones.Text = palabraGuiones.Text.Remove(2 * i, 1).Insert(2 * i, letra);
+                    }
+                }
             }
             else
             {
                 numFallos++;
             }
+
             if (!palabraGuiones.Text.Contains('_'))
             {
                 numFallos = -100;
@@ -58,12 +76,10 @@ namespace Ahorcado_2020
                 case 4: pictureBox1.Image = Properties.Resources.ahorcado_4; break;
                 case 5: pictureBox1.Image = Properties.Resources.ahorcado_5; break;
                 case 6: pictureBox1.Image = Properties.Resources.ahorcado_fin1; break;
-                case -100: pictureBox1.Image = Properties.Resources.homer; break;
-                default: pictureBox1.Image = Properties.Resources.ahorcado_fin; break;
+                case -100: pictureBox1.Image = Properties.Resources.acertastetodo; break;
+                default: pictureBox1.Image = Properties.Resources.ahorcado_fin1; break;
 
             }
         }
-
-        
     }
 }
